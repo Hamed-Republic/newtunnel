3cfa81051e2fd6f7c5eebcf92ceddc94f01a82df
Hamed-Republic <132632530+Hamed-Republic@users.noreply.github.com>
2023-08-06 23:21:06 +0330

    Update app.tsx

--- libs/edge-ui/src/lib/app.tsx
+++ libs/edge-ui/src/lib/app.tsx
@@ -369,7 +369,7 @@ function Warning() {
           />
         </div>
         <div className="ml-3">
-          <h3 className="text-sm font-medium text-red-700">Notice! !</h3>
+          <h3 className="text-sm font-medium text-red-700">Notice!!</h3>
           <div className="mt-2 text-sm text-red-700">
             <p>Disclosing this page is tantamount to disclosing your settings。</p>
           </div>
