14c1977efe5792bb0420c5bab9dfe2138735ca44
Hamed-Republic <132632530+Hamed-Republic@users.noreply.github.com>
2023-08-06 23:25:52 +0330

    Update app.tsx

--- libs/edge-ui/src/lib/app.tsx
+++ libs/edge-ui/src/lib/app.tsx
@@ -344,7 +344,7 @@ function ShareActions({
         type="button"
         className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       >
-        Create Config
+        Create config
       </button>
 
       <button
@@ -369,7 +369,7 @@ function Warning() {
           />
         </div>
         <div className="ml-3">
-          <h3 className="text-sm font-medium text-red-700">Notice!!</h3>
+          <h3 className="text-sm font-medium text-red-700">Notice !!</h3>
           <div className="mt-2 text-sm text-red-700">
             <p>Disclosing this page is tantamount to disclosing your settings。</p>
           </div>
